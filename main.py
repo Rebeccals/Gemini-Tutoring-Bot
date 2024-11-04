ffa1cd7922480e358abc5ae9af8b3f91585a5f68
Nathan <160674481+Nathmathan@users.noreply.github.com>
2024-11-04 15:28:18 -0800

    Edited main to Test Streaming

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from chatbot import *
 
 # Testing out the Gemini API
-response = chat_session.send_message("What's the most popular apple?")
-print(response.text)
+response = chat_session.send_message("What's the most popular apple?",stream=True)
+for chunk in response:
+    print(chunk.text)
+    print("_" * 80)
